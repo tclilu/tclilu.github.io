90c19292729e0324b8f806d437f32ac0a495081e
sunchaser-lilu <admin@lilu.org.cn>
2021-01-14 01:06:58 +0800

    Travis CI Auto Builder at 2021-01-14 01:06

---
+++ js/friend.js
@@ -0,0 +1,43 @@
+$(function () { //获取处理友链数据
+    $.getJSON("../json_data/friend.json", function (data) {
+        let $links = $('.links-content');
+        $links.html("");
+        // 随机排序过滤失效的
+        let notValid = data.filter((item) => item.valid === 0);
+        data = data.filter((item) => item.valid !== 0).sort(function (a, b) {
+            return Math.random() > .5 ? -1 : 1;
+        });
+        $links.append("<div class='friend-title-item'><br>大佬们<br><br><hr></div>");
+        $.each(data, function (i, e) {
+            let html = "<div class='friend-card-item'>";
+            if (e.src === undefined) {
+                html += "    <img class='ava' src='/img/links/nopic.jpg' alt='友链头像' title='图片链接不可用，使用的默认图片'>";
+            } else {
+                html += "    <img class='ava' src='" + e.src + "'>";
+            }
+            html +=
+                "<div class='text-desc' title='" + e.desc + "'>    网址：<a href='" + e.url + "' target='_blank'>" + e.name + "</a>" +
+                "    <br>时间：" + e.date +
+                "<br>简介：" + e.desc + "</div>" +
+                "    </div>";
+
+            $links.append(html);
+        });
+        // 过期的
+        if (notValid.length > 0) {
+            $links.append("<div class='friend-title-item'><br>异常的大佬们<br><br><hr></div></div>");
+            $.each(notValid, function (i, e) {
+                let html = "<div class='friend-card-item'>";
+                html += "    <img class='ava' src='/img/links/nopic.jpg' alt='友链头像' title='图片链接不可用，使用的默认图片'>";
+                html +=
+                    "<div class='text-desc' title='"+e.desc+"'>    网址：<a href='" + e.url + "' target='_blank'>" + e.name + "</a>" +
+                    "    <br>访问时间：" + e.stopTime +
+                    "<br>简介：" + e.desc + "</div>" +
+                    "    </div>";
+
+                $links.append(html);
+            })
+        }
+        $links.append("</div>");
+    })
+});
---
+++ js/theme.js
@@ -0,0 +1,99 @@
+Storage.prototype.setExpire = (key, value, expire) => {
+    let obj = {
+        data: value,
+        time: Date.now(),
+        expire: expire
+    };
+    localStorage.setItem(key, JSON.stringify(obj));
+}
+
+Storage.prototype.getExpire = key => {
+    let val = localStorage.getItem(key);
+    if (!val) {
+        return val;
+    }
+    val = JSON.parse(val);
+    if (Date.now() - val.time > val.expire) {
+        localStorage.removeItem(key);
+        return null;
+    }
+    return val.data;
+}
+let expireTime1H = 1000 * 60 * 60; // 1小时过期
+function isNightRange(beginTime, endTime) {
+    let nowDate = new Date();
+    let nowTime = nowDate.getHours() + ":" + nowDate.getMinutes();
+    let strb = beginTime.split(":");
+    if (strb.length !== 2) {
+        return false;
+    }
+    let stre = endTime.split(":");
+    if (stre.length !== 2) {
+        return false;
+    }
+    let strn = nowTime.split(":");
+    if (stre.length !== 2) {
+        return false;
+    }
+    let b = new Date();
+    let e = new Date();
+    let n = new Date();
+    b.setHours(strb[0]);
+    b.setMinutes(strb[1]);
+    e.setHours(stre[0]);
+    e.setMinutes(stre[1]);
+    n.setHours(strn[0]);
+    n.setMinutes(strn[1]);
+    return n.getTime() - b.getTime() > 0 && n.getTime() - e.getTime() < 0;
+}
+
+function isNightFun() {
+    let isNightTemp = localStorage.getExpire('night');
+    // 第一次进来判断是白天还是晚上
+    if (isNightTemp === null || isNightTemp === undefined) {
+        isNightTemp = isNightRange("19:00", "23:59") || isNightRange("00:00", "07:00") ? 'true' : 'false';
+        localStorage.setExpire("night", isNightTemp, expireTime1H);
+    }
+    return isNightTemp;
+}
+let isNight=isNightFun();
+// 参考自 https://www.imaegoo.com/
+let nightNav;
+let nightIcon;
+
+function applyNight(value) {
+    if (value === 'true') {
+        document.body.className += ' night'
+        if (nightIcon) {
+            nightIcon.className = nightIcon.className.replace(/ fa-moon/g, '') + ' fa-lightbulb'
+        }
+    } else {
+        document.body.className = document.body.className.replace(/ night/g, '')
+        if (nightIcon) {
+            nightIcon.className = nightIcon.className.replace(/ fa-lightbulb/g, '') + ' fa-moon'
+        }
+    }
+}
+
+function findNightIcon() {
+    nightNav = document.getElementById('night-nav');
+    nightIcon = document.getElementById('night-icon');
+    if (!nightNav || !nightIcon) {
+        setTimeout(findNightIcon, 100);
+    } else {
+        nightNav.addEventListener('click', switchNight);
+        nightIcon.className = isNight ? nightIcon.className.replace(/ fa-moon/g, '') + ' fa-lightbulb' : nightIcon.className.replace(/ fa-lightbulb/g, '') + ' fa-moon';
+    }
+}
+
+function switchNight() {
+    isNight = isNight === 'false' ? 'true' : 'false';
+    applyNight(isNight);
+    localStorage.setExpire('night', isNight, expireTime1H);
+    if(typeof loadUtterances == 'function'){
+        loadUtterances();
+    }
+}
+
+findNightIcon();
+applyNight(isNight);
