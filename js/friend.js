5718e86b73f06453f8f2010e6e99cea793ec532d
sunchaser-lilu <admin@lilu.org.cn>
2021-01-27 02:35:59 +0800

    Travis CI Auto Builder at 2021-01-27 02:35

--- js/friend.js
+++ js/friend.js
@@ -13,7 +13,7 @@ $(function () { //获取处理友链数据
             if (e.src === undefined) {
                 html += "    <img class='ava' src='https://cdn.jsdelivr.net/gh/sunchaser-lilu/sunchaser-cdn@master/lilu.org.cn/avatar.png' alt='友链头像' title='图片链接不可用，使用的默认图片'>";
             } else {
-                html += "    <img class='ava' src='" + e.src + "'>";
+                html += '    <img class="ava" src="' + e.src + '" onerror="handleImgError();">';
             }
             html +=
                 "<div class='text-desc' title='" + e.desc + "'>    网址：<a href='" + e.url + "' target='_blank'>" + e.name + "</a>" +
@@ -28,7 +28,7 @@ $(function () { //获取处理友链数据
             $links.append("<div class='friend-title-item'><br>异常的大佬们<br><br><hr></div></div>");
             $.each(notValid, function (i, e) {
                 let html = "<div class='friend-card-item'>";
-                html += "    <img class='ava' src='/img/links/nopic.jpg' alt='友链头像' title='图片链接不可用，使用的默认图片'>";
+                html += "    <img class='ava' src='https://cdn.jsdelivr.net/gh/sunchaser-lilu/sunchaser-cdn@master/lilu.org.cn/avatar.png' alt='友链头像' title='图片链接不可用，使用的默认图片'>";
                 html +=
                     "<div class='text-desc' title='"+e.desc+"'>    网址：<a href='" + e.url + "' target='_blank'>" + e.name + "</a>" +
                     "    <br>访问时间：" + e.stopTime +
@@ -41,3 +41,8 @@ $(function () { //获取处理友链数据
         $links.append("</div>");
     })
 });
+function handleImgError() {
+    let img = event.srcElement;
+    img.src = "https://cdn.jsdelivr.net/gh/sunchaser-lilu/sunchaser-cdn@master/lilu.org.cn/avatar.png";
+    img.onerror=null;
+}
