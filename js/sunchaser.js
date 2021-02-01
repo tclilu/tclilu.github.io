59cbf85b77431d4f75db15b8de661ed2cb05ba4e
sunchaser-lilu <admin@lilu.org.cn>
2021-02-02 04:37:18 +0800

    Travis CI Auto Builder at 2021-02-02 04:37

--- js/sunchaser.js
+++ js/sunchaser.js
@@ -7,22 +7,19 @@ window.addEventListener('load', function (event) {
 });
 
 // 导航栏夜间模式切换事件
-const changeMode = function () {
-    document.getElementById('darkmode').addEventListener('click', function (e) {
-        const nowMode = document.documentElement.getAttribute('data-theme') === 'dark' ? 'dark' : 'light';
-        if (nowMode === 'light') {
-            activateDarkMode();
-            saveToLocal.set('theme', 'dark', 2);
-            GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night);
-        } else {
-            activateLightMode();
-            saveToLocal.set('theme', 'light', 2);
-            GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.night_to_day);
-        }
-        // handle some cases
-        typeof utterancesTheme === 'function' && utterancesTheme();
-        typeof FB === 'object' && window.loadFBComment();
-        window.DISQUS && document.getElementById('disqus_thread').children.length && setTimeout(() => window.disqusReset(), 200);
-    });
-};
-changeMode();
+document.getElementById('darkmode').addEventListener('click', function (e) {
+    const nowMode = document.documentElement.getAttribute('data-theme') === 'dark' ? 'dark' : 'light';
+    if (nowMode === 'light') {
+        activateDarkMode();
+        saveToLocal.set('theme', 'dark', 2);
+        GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night);
+    } else {
+        activateLightMode();
+        saveToLocal.set('theme', 'light', 2);
+        GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.night_to_day);
+    }
+    // handle some cases
+    typeof utterancesTheme === 'function' && utterancesTheme();
+    typeof FB === 'object' && window.loadFBComment();
+    window.DISQUS && document.getElementById('disqus_thread').children.length && setTimeout(() => window.disqusReset(), 200);
+});
